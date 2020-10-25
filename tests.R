c04ee8afccb1d6863bdea6037e63002076d8475a
George Kazantzidis <georgios.kazantzidis@uzh.ch>
2020-10-25 19:16:03 +0100

    corrections

--- tests.R
+++ tests.R
@@ -1,7 +1,7 @@
 source(file = "my_fun.R", local = TRUE)
 
 #### even_odd #
-# This fucntion determines whether a number is even or odd. Also works with vectors
+# This function determines whether a number is even or odd. Also works with vectors
 
 even_odd(3)
 even_odd(4)
@@ -10,7 +10,7 @@ even_odd(vec)
 
 
 #### all_fac #
-# This fucntion finds all the factors of a number. Returns both negative and positive factors 
+# This function finds all the factors of a number. Returns both negative and positive factors 
 
 all_fac(6)
 all_fac(-7)
@@ -26,7 +26,7 @@ all_fac_l(li)
 
 
 #### my.lm # 
-# This fucntion takes a data.frame with variables x and y, estimates the linear regression model
+# This function takes a data.frame with variables x and y, estimates the linear regression model
 # Returns the p-values of the parameters
 
 df <- data.frame(x = runif(100, 0, 5))
@@ -35,7 +35,7 @@ my.lm(df)
 
 
 #### my.test #
-# This fucntion calculates the mean difference between two variables x and y from a training set and uses this mean difference
+# This function calculates the mean difference between two variables x and y from a training set and uses this mean difference
 # to predict the viable y given the variable x in a second, evaluation data set
 # Returns a list with the fitted values (1rst col) of the first, training data set and their residuals (2nd col)
 # Also returns the predicted values (3nd col) for the second, evaluation data set and their errors (4rth col)
